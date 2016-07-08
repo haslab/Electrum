61188c420a9edb8c2676f2e65f20a720f6a1671b
EduardoPessoa <eduardo.jose.pessoa@gmail.com>
2016-07-08 15:42:30 +0100

    fixed bug on hotel example

--- src/ElectrumUnitTesting/UnitTests/Hotel.java
+++ src/ElectrumUnitTesting/UnitTests/Hotel.java
@@ -33,7 +33,7 @@ public class Hotel {
         opt.noOverflow = true;
         opt.skolemDepth = 1;
         rep = new SimpleReporter();
-        path = "./src/ElectrumUnitTesting/models/ring.ele";
+        path = "./src/ElectrumUnitTesting/models/hotel.ele";
         cp = CompUtil.parseEverything_fromFile(rep, null, path);
 
     }
