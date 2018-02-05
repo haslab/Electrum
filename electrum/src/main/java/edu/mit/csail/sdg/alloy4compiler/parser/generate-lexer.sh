e9e5cbc408de306e3ad6659d600189807c12be3c
nmacedo <nfmmacedo@di.uminho.pt>
2018-02-05 13:25:29 +0000

    added train examples

--- electrum/src/main/java/edu/mit/csail/sdg/alloy4compiler/parser/generate-lexer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-java -jar ../../../../../../../lib/jflex-1.6.0.jar --nobak -d . Alloy.lex
-
-sed -i 's/public java_cup.runtime.Symbol next_token() throws java.io.IOException/public java_cup.runtime.Symbol next_token() throws java.io.IOException, Err/' CompLexer.java
-
