058ddb4468458de5752794f280082b2d704af7db
nmacedo <nfmmacedo@di.uminho.pt>
2019-07-10 14:52:39 +0100

    updated cup parser

--- electrum/src/main/java/java_cup/runtime/lr_parser.java
+++ electrum/src/main/java/java_cup/runtime/lr_parser.java
@@ -311,10 +311,10 @@ public abstract class lr_parser {
    * @param top       the index of the top element of the parse stack.
    */
   public abstract Symbol do_action(
-    int       act_num, 
-    lr_parser parser, 
-    Stack     stack, 
-    int       top) 
+    int           act_num, 
+    lr_parser     parser, 
+    Stack<Symbol> stack, 
+    int           top) 
     throws java.lang.Exception;
 
   /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
